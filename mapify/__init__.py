061529f3264242f2163bc956a5e5edee09ed649e
RubeMeuleman <r0990226@student.thomasmore.be>
2024-08-09 21:23:07 +0200

    Version 1.0.0 - Readme and scripts optimized

--- mapify/__init__.py
+++ mapify/__init__.py
@@ -1,5 +1,5 @@
-from .encode import encode
-from .decode import decode
+from .encrypt import encrypt
+from .decrypt import decrypt
 from .generate import generate
 
-__all__ = ['encode', 'decode', 'generate']
+__all__ = ['encrypt', 'decrypt', 'generate']
